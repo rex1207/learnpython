ffcc49f2330939116ac07e3cf83c2eaada22febf
kingofhawks <kingofhawks@qq.com>
2020-08-09 11:58:38 +0800

    add fatherandson sub module

---
+++ fatherandson/ch8/cfb.py
@@ -0,0 +1,5 @@
+# 输出乘法表
+a=int(input('要我输入啥'))
+b=int(input('你要乘几'))
+for i in range(b):
+    print(a,'x',1+i,'=',a*(i+1))
---
+++ fatherandson/ch8/cfb1.py
@@ -0,0 +1,6 @@
+# 输出乘法表2
+a=int(input('要我输入啥'))
+b=int(input('你要乘几'))
+for i in range(1,b+1):
+    print(a,'x',i,'=',a*i)
+    
